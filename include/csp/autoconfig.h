5a522d957df4da43ca7b74fee2b26c74f8293265
Saverio Nasturzio <saverio@dphispace.com>
2025-07-02 19:09:50 +0200

    changed autoconfigs

--- include/csp/autoconfig.h
+++ include/csp/autoconfig.h
@@ -12,21 +12,20 @@
 
 //#cmakedefine01 CSP_REPRODUCIBLE_BUILDS
 
-//TODO: define the following
 //#cmakedefine CSP_QFIFO_LEN @CSP_QFIFO_LEN@
 #define CSP_QFIFO_LEN 16
 //#cmakedefine CSP_PORT_MAX_BIND @CSP_PORT_MAX_BIND@
-#define CSP_PORT_MAX_BIND 2
+#define CSP_PORT_MAX_BIND 32
 //#cmakedefine CSP_CONN_RXQUEUE_LEN @CSP_CONN_RXQUEUE_LEN@
 #define CSP_CONN_RXQUEUE_LEN 4
 //#cmakedefine CSP_CONN_MAX @CSP_CONN_MAX@
-#define CSP_CONN_MAX 2
+#define CSP_CONN_MAX 1
 //#cmakedefine CSP_BUFFER_SIZE @CSP_BUFFER_SIZE@
-#define CSP_BUFFER_SIZE 128
+#define CSP_BUFFER_SIZE 256
 //#cmakedefine CSP_BUFFER_COUNT @CSP_BUFFER_COUNT@
 #define CSP_BUFFER_COUNT 8
 //#cmakedefine CSP_RDP_MAX_WINDOW @CSP_RDP_MAX_WINDOW@
-#define CSP_RDP_MAX_WINDOW 8 // check this if you use RDP
+#define CSP_RDP_MAX_WINDOW 1 // check this if you use RDP
 //#cmakedefine CSP_RTABLE_SIZE @CSP_RTABLE_SIZE@
 #define CSP_RTABLE_SIZE 4
 
