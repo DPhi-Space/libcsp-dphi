55353a2f75b51355eb6b11e449152400dbaea92e
Mickael Bonjour <mickael@dphispace.com>
2025-08-18 08:46:30 +0200

    feat: added skipping encryption in case of an error

--- src/interfaces/csp_if_tun.c
+++ src/interfaces/csp_if_tun.c
@@ -9,11 +9,15 @@
 #define ASCON_NONCE_BYTES 16
 
 int csp_crypto_decrypt(uint8_t * ciphertext_in, uint8_t ciphertext_len, uint8_t * msg_out) {
-	csp_print("CRYPTO DECRYPT\r\n");
-	csp_print_packet()
+	// Prepare buffer for 16bytes Nonce
 	unsigned char n[ASCON_NONCE_BYTES];
 	memset(n, 0, ASCON_NONCE_BYTES);
 
+	if (ciphertext_len < ASCON_NONCE_BYTES) {
+		csp_print("The incoming encrypted message is shorter than expected, will skip decryption");
+		memcpy(ciphertext_in, msg_out, ciphertext_len);
+		return ciphertext_len;
+	}
 	// Copy Nonce from incoming message
 	memcpy(n, ciphertext_in, ASCON_NONCE_BYTES);
 	// Adapt lengths and move pointer
@@ -27,18 +31,22 @@ int csp_crypto_decrypt(uint8_t * ciphertext_in, uint8_t ciphertext_len, uint8_t 
 	unsigned long long mlen = 0;
 	int result = 0;
 
+	// Ascon decryption and get result
   	result |= crypto_aead_decrypt(msg_out, &mlen, (void*)0, ciphertext_in, ciphertext_len, NULL, alen, n, k);
 	if(result) {
 		csp_print("Error in decryption... Error : %i\n", result);
-		return -1;
+		// As there was an error but no means to tell CSP, we can copy original message directly
+		ciphertext_in -= ASCON_NONCE_BYTES;
+		ciphertext_len += ASCON_NONCE_BYTES;
+		memcpy(ciphertext_in, msg_out, ciphertext_len);
+		return ciphertext_len;
 	}
-	csp_print("The returned decrypted length is %u compared to initial length : %u\n", mlen, ciphertext_len);
+
   	return mlen;
 }
 
 int csp_crypto_encrypt(uint8_t * msg_begin, uint8_t msg_len, uint8_t * ciphertext_out) {
-	csp_print("CRYPTO ENCRYPT\r\n");
-
+	// Prepare the buffer for Nonce
 	unsigned char n[ASCON_NONCE_BYTES];
 	memset(n, 0, ASCON_NONCE_BYTES);
 
@@ -59,17 +67,20 @@ int csp_crypto_encrypt(uint8_t * msg_begin, uint8_t msg_len, uint8_t * ciphertex
 	unsigned long long clen = 0;
 	int result = 0;
 
-	// TODO Check on packet limit size ?
+	// TODO: Check on packet limit size ?
 	result |= crypto_aead_encrypt(ciphertext_out, &clen, msg_begin, msg_len, NULL, alen, (void*)0, n, k);
-	if(result)
-	{
-		csp_print("Error in decryption... Error : %i\n", result);
-		return -1;
+	if(result) {
+		// No ways to tell CSP there was an error, we then return the unencrypted frame
+		ciphertext_out -= ASCON_NONCE_BYTES;
+		memset(ciphertext_out, 0, ASCON_NONCE_BYTES);
+
+		memcpy(msg_begin, ciphertext_out, msg_len);
+		csp_print("Error in encryption... Error : %i\n", result);
+		return msg_len;
 	}
 
 	// Add the prepended Nonce length
 	clen += ASCON_NONCE_BYTES;
-	csp_print("The returned encrypted length is %u compared to initial length : %u\n", clen, msg_len);
 	return clen;
 }
 
@@ -89,7 +100,7 @@ static int csp_if_tun_tx(csp_iface_t * iface, uint16_t via, csp_packet_t * packe
 		/**
 		 * Incomming tunnel packet
 		 */
-		csp_hex_dump("incoming packet", packet->data, packet->length);
+		//csp_hex_dump("incoming packet", packet->data, packet->length);
 
 		csp_id_setup_rx(new_packet);
 
@@ -116,7 +127,7 @@ static int csp_if_tun_tx(csp_iface_t * iface, uint16_t via, csp_packet_t * packe
 
 		csp_id_strip(new_packet);
 
-		csp_hex_dump("new packet", new_packet->data, new_packet->length);
+		//csp_hex_dump("new packet", new_packet->data, new_packet->length);
 
 		/* Send new packet */
 		csp_qfifo_write(new_packet, iface, NULL);
@@ -132,7 +143,7 @@ static int csp_if_tun_tx(csp_iface_t * iface, uint16_t via, csp_packet_t * packe
 		/* Apply CSP header */
 		csp_id_prepend(packet);
 
-		csp_hex_dump("outgoing frame", packet->frame_begin, packet->frame_length);
+		//csp_hex_dump("outgoing frame", packet->frame_begin, packet->frame_length);
 
 		/* Create tunnel header */
 		new_packet->id.dst = ifconf->tun_dst;
@@ -158,7 +169,7 @@ static int csp_if_tun_tx(csp_iface_t * iface, uint16_t via, csp_packet_t * packe
 		/* Apply CSP header */
 		csp_id_prepend(new_packet);
 
-		csp_hex_dump("outgoing new frame", new_packet->frame_begin, new_packet->frame_length);
+		//csp_hex_dump("outgoing new frame", new_packet->frame_begin, new_packet->frame_length);
 
 		/* Send new packet */
 		csp_qfifo_write(new_packet, iface, NULL);
